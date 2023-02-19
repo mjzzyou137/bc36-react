5e0b46e8db7bc928759e738ab7325deebfd8b0a6
Man Ng <>
2023-02-19 16:01:02 +0700

    init

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const TOKEN_CYBERSOFT = process.env.REACT_APP_TOKEN_CYBERSOFT;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const GROUP_ID = process.env.REACT_APP_GROUP_ID;
